e4aa3a0288fec15af307bc9aff4982b189337b0a
KHUSH PATEL <110011276+khushpatel2121@users.noreply.github.com>
2024-07-04 17:51:14 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from joblib import load
+import os
 
 # Load the trained model
 model = load('diabetes_model.joblib')
@@ -13,8 +14,11 @@ def predict():
     data = request.get_json(force=True)
     features = [data['Pregnancies'], data['Glucose'], data['BloodPressure'], data['SkinThickness'], data['Insulin'], data['BMI'], data['DiabetesPedigreeFunction'], data['Age']]
     prediction = model.predict([features])
-    result = 'Diabatic' if prediction[0] == 1 else 'Non-Diabatic'
+    result = 'Diabetic' if prediction[0] == 1 else 'Non-Diabetic'
     return jsonify({'prediction': result})
 
+# Use environment variables to configure host and port for deployment flexibility
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Use PORT environment variable if defined, otherwise default to 5000
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
