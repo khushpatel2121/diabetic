080bd9e256d0274325f2198c0e4b098b411bb57a
KHUSH PATEL <110011276+khushpatel2121@users.noreply.github.com>
2024-07-04 17:53:35 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Response
 from flask_cors import CORS
 from joblib import load
 import os
@@ -9,18 +9,28 @@ model = load('diabetes_model.joblib')
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/predict', methods=['POST'])
+@app.route('/predict', methods=['POST', 'OPTIONS'])
 def predict():
+    if request.method == 'OPTIONS':
+        # Handle preflight request for CORS
+        headers = {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "Content-Type,Authorization",
+            "Access-Control-Allow-Methods": "GET,POST,OPTIONS"
+        }
+        return Response(headers=headers)
+
     data = request.get_json(force=True)
     features = [data['Pregnancies'], data['Glucose'], data['BloodPressure'], data['SkinThickness'], data['Insulin'], data['BMI'], data['DiabetesPedigreeFunction'], data['Age']]
     prediction = model.predict([features])
     result = 'Diabetic' if prediction[0] == 1 else 'Non-Diabetic'
 
+    response = jsonify({'prediction': result})
     response.headers.add("Access-Control-Allow-Origin", "*")
     response.headers.add("Access-Control-Allow-Headers", "Content-Type,Authorization")
     response.headers.add("Access-Control-Allow-Methods", "GET,POST,OPTIONS")
 
-    return jsonify({'prediction': result})
+    return response
 
 # Use environment variables to configure host and port for deployment flexibility
 if __name__ == '__main__':
